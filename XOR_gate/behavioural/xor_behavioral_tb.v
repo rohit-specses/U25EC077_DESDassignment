e9fa05e57139d5c799982806683e228f74de1a28
rohit-specses <rohitlgohil295@gmail.com>
2026-06-11 00:22:31 +0530

    Add files via upload

---
+++ XOR_gate/behavioural/xor_behavioral_tb.v
@@ -0,0 +1,15 @@
+module testbench;
+    reg a, b;
+    wire y;
+
+    xor_using_nand uut (.a(a), .b(b), .y(y));
+
+    initial begin
+        $monitor("a=%b b=%b y=%b", a, b, y);
+        #10 a=0; b=0;
+        #10 a=0; b=1;
+        #10 a=1; b=0;
+        #10 a=1; b=1;
+        #10 $finish;
+    end
+endmodule
